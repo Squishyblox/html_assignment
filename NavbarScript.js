3d6fe59a6b03c6788d2bc1342a742e8a967dcd2f
Squishyblox <nghongwee17@gmail.com>
2022-06-23 10:59:11 +0800

    upload

---
+++ NavbarScript.js
@@ -0,0 +1,23 @@
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("pot").style.top = "40px";
+        } else {
+            document.getElementById("pot").style.top = "-20px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+
+    window.onresize = function() {
+        document.getElementById("mySidepanel").style.width = "0";
+    }
+
+    function sideNav(){
+        document.getElementById("mySidepanel").style.width = "250px";
+    }
+
+    function closeNav(){
+        document.getElementById("mySidepanel").style.width = "0";
+    }
